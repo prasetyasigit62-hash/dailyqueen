1c97ecf389067792eb38daa14ec229b002bce908
Sigit Prasetya <prasetyasigit62@gmail.com>
2026-06-04 08:55:33 +0700

    Tenant detail: remove Queen City Mall suffix from location line

--- app/(primaryLayout)/_section/mallDirectory/TenantDetailView.tsx
+++ app/(primaryLayout)/_section/mallDirectory/TenantDetailView.tsx
@@ -859,7 +859,7 @@ export default function TenantDetailView({ tenant, onBack }: TenantDetailViewPro
                                         <IconPin />
                                     </span>
                                     <span>
-                                        {locationFloor} | {mallName}
+                                        {locationFloor}
                                     </span>
                                 </div>
 
